609f5dc23e7d21e217c4b47bc476f122ca41e07b
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-28 21:54:07 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,15 @@
-# Write your code here
+class GetRequester
+  
+  def initialize(url)
+  end
+  
+  def get_response_body #sends a GET request to the URL passed in on initialization, also should return the body of the response
+  end
+  
+  def parse_json #should use method get_response_body to send a request, then return an array or hash made up of data 
+  end
+  
+  
+  
+  
+end
